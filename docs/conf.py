e5e156a86188398425ee46004ce7a68235332c04
duypham2108 <phamduychv94@gmail.com>
2021-03-28 01:01:25 +1000

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../stlearn'))
 sys.path.insert(0, os.path.abspath('../stlearn/spatials'))
 sys.path.insert(0, os.path.abspath('../stlearn/spatials/clustering'))
 sys.path.insert(0, os.path.abspath('../stlearn/spatials/morphology'))
